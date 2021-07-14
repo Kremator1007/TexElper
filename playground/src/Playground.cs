71d36506201e41eb5a910db45044bf0f74d7c05e
James <js7572305@gmail.com>
2021-07-14 16:05:20 +0300

    First implementations are ready!

---
+++ playground/src/Playground.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Playground
+{
+    public static void Main()
+    {
+        string path = "media\\b.tex";
+        var file = new System.IO.FileInfo(path);
+        var t = FileProcessor.ReadFile(file);
+        foreach (Problem problem in t)
+        {
+            Console.WriteLine();
+            Console.WriteLine(problem.Text);
+        }
+    }
+}
