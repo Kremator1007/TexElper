702a2bc8fb5d4f8cfd3cbe55e25c945baeebfc7e
James <js7572305@gmail.com>
2021-07-17 12:35:41 +0300

    Reduced away magic number

--- program/src/BackendFileComparer/ProblemComparer/ByLevenshteinProblemComparer.cs
+++ program/src/BackendFileComparer/ProblemComparer/ByLevenshteinProblemComparer.cs
@@ -2,9 +2,10 @@ public class ByLevenshteinProblemComparer : IProblemComparer
 {
     public bool AreTwoProblemsSimilar(Problem fst, Problem snd)
     {
-        if (System.Math.Abs(fst.Text.Length - snd.Text.Length) >= 42)
+        if (System.Math.Abs(fst.Text.Length - snd.Text.Length) >= comparingSensitivity)
             return false;
         else
-            return Tools.LevenshteinDistance(fst.Text, snd.Text) < 42;
+            return Tools.LevenshteinDistance(fst.Text, snd.Text) < comparingSensitivity;
     }
+    private readonly int comparingSensitivity = 42;
 }
