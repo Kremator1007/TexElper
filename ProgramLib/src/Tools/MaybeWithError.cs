0cb67ca5cb155725eac40ea1e92f2104e2729d27
James <js7572305@gmail.com>
2021-07-21 15:09:34 +0300

    Used switch expression as a better alternative

--- ProgramLib/src/Tools/MaybeWithError.cs
+++ ProgramLib/src/Tools/MaybeWithError.cs
@@ -2,15 +2,13 @@ public class MaybeWithError<ValueT, ErrorT>
 {
     public MaybeWithError<ValueT2, ErrorT> Bind<ValueT2>(
         System.Func<ValueT,
-        MaybeWithError<ValueT2, ErrorT>> func)
-    {
-        if (this is ValueWrapper<ValueT, ErrorT> valueWrapper)
-            return func(valueWrapper.Value);
-        else if (this is ErrorWrapper<ValueT, ErrorT> errorWrapper)
-            return new ErrorWrapper<ValueT2, ErrorT>(errorWrapper.Error);
-        else
-            throw new System.Exception("Unreachable");
-    }
+        MaybeWithError<ValueT2, ErrorT>> func) => this switch
+        {
+            ValueWrapper<ValueT, ErrorT> valueWrapper => func(valueWrapper.Value),
+            ErrorWrapper<ValueT, ErrorT> errorWrapper =>
+                new ErrorWrapper<ValueT2, ErrorT>(errorWrapper.Error),
+            _ => throw new System.Exception("Unreachable")
+        };
 }
 
 public class ValueWrapper<ValueT, ErrorT> : MaybeWithError<ValueT, ErrorT>
